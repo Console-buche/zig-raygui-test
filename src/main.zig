a3569327de38e0fff091413a7aea93fca107d80b
CodeMyst <code@myst.rs>
2023-10-23 13:36:38 +0200

    init

---
+++ src/main.zig
@@ -0,0 +1,32 @@
+const rl = @cImport({
+    @cDefine("RAYGUI_IMPLEMENTATION", {});
+    @cInclude("raylib.h");
+    @cInclude("raygui.h");
+});
+
+pub fn main() !void {
+    rl.InitWindow(1280, 720, "yeet");
+    defer rl.CloseWindow();
+
+    rl.SetTargetFPS(60);
+
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+
+        rl.ClearBackground(rl.RAYWHITE);
+
+        if (rl.GuiButton(
+            rl.Rectangle{
+                .x = 0,
+                .y = 0,
+                .width = 100,
+                .height = 100,
+            },
+            "yeet",
+        ) == 1) {
+            // ...
+        }
+
+        rl.EndDrawing();
+    }
+}
